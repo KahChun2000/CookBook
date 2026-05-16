c5b5c9882d9e27927684c516aace9ae8c1556aba
KC <kahchunliew@fusionqb.com>
2026-05-17 00:48:07 +0800

    Initialize IME insets in MainActivity onCreate

--- android/app/src/main/java/com/cookbook/MainActivity.kt
+++ android/app/src/main/java/com/cookbook/MainActivity.kt
@@ -13,25 +13,20 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
   override fun getMainComponentName(): String = "CookBook"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 
-  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-    setupImeInsets()
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      setupImeInsets()
+    }
   }
-}
 
-private fun setupImeInsets() {
+  private fun setupImeInsets() {
     val rootView = findViewById<View>(android.R.id.content)
 
     ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
@@ -48,3 +43,4 @@ private fun setupImeInsets() {
       insets
     }
   }
+}
